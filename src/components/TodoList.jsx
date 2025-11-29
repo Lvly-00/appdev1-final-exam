025b85c9ead16eb1bd4f76c7349aa5a5075100d5
Lovely <128210631+Lvly-00@users.noreply.github.com>
2025-11-29 14:20:53 +0800

    Implement TodoList component

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,22 +1,22 @@
-// import React from "react";
-// import TodoItem from "./TodoItem";
+import React from "react";
+import TodoItem from "./TodoItem";
 
-// function TodoList({ todos, theme, toggleComplete, deleteTodo }) {
-//   return (
-//     <div id="myUnOrdList">
-//       <ul className="todo-list">
-//         {todos.map((todo, index) => (
-//           <TodoItem
-//             key={index}
-//             todo={todo}
-//             theme={theme}
-//             onToggleComplete={() => toggleComplete(index)}
-//             onDelete={() => deleteTodo(index)}
-//           />
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
+function TodoList({ todos, theme, toggleComplete, deleteTodo }) {
+  return (
+    <div id="myUnOrdList">
+      <ul className="todo-list">
+        {todos.map((todo, index) => (
+          <TodoItem
+            key={index}
+            todo={todo}
+            theme={theme}
+            onToggleComplete={() => toggleComplete(index)}
+            onDelete={() => deleteTodo(index)}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
 
-// export default TodoList;
+export default TodoList;
